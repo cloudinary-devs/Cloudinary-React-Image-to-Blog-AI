77a4c53690b0101c82945670cd341508e4734c9e
Patricio Vargas <patricio.vargas@cloudinary.com>
2024-05-28 15:06:25 +0200

    deesign and image and caption working

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css'
+import { AdvancedImage } from '@cloudinary/react';
+import { fill } from '@cloudinary/url-gen/actions/resize';
+import { Cloudinary } from '@cloudinary/url-gen/index';
 
 const ImageUpload = () => {
   const [image, setImage] = useState(null);
   const [caption, setCaption] = useState('');
   const [error, setError] = useState('');
+  const [shouldSubmit, setShouldSubmit] = useState(false);
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'ai-devx-demo'
+    }
+  });
 
-  const handleImageChange = (e) => {
-    setImage(e.target.files[0]);
+
+  useEffect(() => {
+    if (shouldSubmit && image) {
+      handleSubmit();
+    }
+  }, [shouldSubmit]);
+
+  const handleImageChange = async (e) => {
+    console.log(e.target.files[0]);
+    if (e.target.files[0] !== null) {
+      setImage(e.target.files[0]);
+      setShouldSubmit(true);
+    }
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
     if (!image) {
       alert('Please select an image to upload');
+      setShouldSubmit(false);
       return;
     }
 
@@ -27,22 +48,32 @@ const ImageUpload = () => {
         },
       });
       setCaption(response.data.info.detection.captioning.data.caption);
+      const myImage = cld.image(response.data.public_id); 
+
+      // Resize to 250 x 250 pixels using the 'fill' crop mode.
+       myImage.resize(fill().width(500).height(500));
+      setImage(myImage)
       setError(''); // Clear any previous error messages
     } catch (error) {
       console.error('Error uploading image:', error);
       setError('Error uploading image: ' + error.message);
+    } finally {
+      setShouldSubmit(false);
     }
   };
 
   return (
-    <div>
-      <h1>Image Upload</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="file" accept="image/*" onChange={handleImageChange} />
-        <button type="submit">Upload</button>
+    <div className="app">
+      <h1>Image to Blog AI</h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+      <label className="custom-file-upload">
+          <input type="file" accept="image/*" onChange={handleImageChange} />
+          Choose File
+        </label>
       </form>
       {error && <p style={{ color: 'red' }}>{error}</p>}
-      {caption && <p>Caption: {caption}</p>}
+      <AdvancedImage cldImg={image} alt={caption}/>
+      {caption && <p>{caption}</p>}
     </div>
   );
 };
